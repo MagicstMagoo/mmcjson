f75e600ff50530c3355f5b62ca58c407acdaaadf
麻瓜瓜 <96295450+MagicstMagoo@users.noreply.github.com>
2022-05-21 19:00:24 +0800

    upgrade somethings :)

--- include/json.hpp
+++ include/json.hpp
@@ -6,9 +6,42 @@
 #include​ ​<​optional​> 
 #​include​ ​<​ostream​> 
 #​include​ ​<​string​>
+//还可以用<QString>读取Minecraft的json文件
 ​#​include​ ​<​unordered_map​> 
 ​#​include​ ​<​vector​> 
 ​#​include​ ​<​tuple​> 
 ​#​include​ ​<​variant​> 
 #​include​ ​<​string_view​> 
-                                         
+
+//inline常量设置
+#define MMCJSON_INLINE inline
+
+namespace json 
+{
+	class array;
+	class object;
+
+	//value declare
+	class value;
+	{
+		using array_ptr = std::unique_ptr<array>;
+		using onject_ptr = std::unique_ptr<object>;
+
+	public:
+		enum class value_type : char
+		{
+			Invalid,
+			Null,
+			Boolean,
+			String,
+			Number,
+			Array,
+			Object,
+		};
+
+		using var_t = std::variant<std::string, array_ptr, object_ptr>;
+
+	public:
+		//value
+	}
+}
