26636294e3f9358d9c850f86872d6230de5338c3
麻瓜瓜 <96295450+MagicstMagoo@users.noreply.github.com>
2022-05-29 08:53:30 +0800

    Complete sample

--- sample/sample.cpp
+++ sample/sample.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <vector>
 #include <set>
+#include <vector>
 #include <map>
+
+// using json.hpp
 #include "json.hpp"
 
 void parsing();
@@ -9,39 +11,125 @@ void serializing();
 
 int main()
 {
-	std::cout << "\nParsing\n" << std::endl;
+    std::cout << "\n****** Parsing ******\n" << std::endl;
 
-	parsing();
+    parsing();
 
-	std::cout << "\nSerializing\n" << std::endl;
+    std::cout << "\n****** Serializing ******\n" << std::endl;
 
-	serializing();
+    serializing();
 
-	return 0;
+    return 0;
 }
 
 void parsing()
 {
-	std::string content = "R"()
-	{
-		"repo":"mmcjson",
-		"author" : {
-		"Magicst_Magoo":"https://github.com/MagicstMagoo"},
-		"list" : [
-				1,
-				2,
-				3
-				],
-		"str":"abc",
-		"num":3.1416,
-		"A_obj" : {
-		"B_obj": [
-					{
-						"C_obj":"hey you found me"
-					}
-				]
-			}
-		}
-	)";
+    std::string content = R"(
+{
+    "repo": "mmcjson",
+    "author": {
+        "MagicstMagoo": "https://github.com/MagicstMagoo"
+    },
+    "list": [
+        1,
+        2,
+        3
+    ],
+    "str": "abc",
+    "num": 3.1416,
+    "A_obj": {
+        "B_arr": [
+            {
+                "C_str": "you found me!"
+            }
+        ]
+    }
+}
+    )";
 
+    auto ret = json::parse(content);
+
+    if (!ret) {
+        std::cerr << "Parsing failed" << std::endl;
+        return;
+    }
+    auto& value = ret.value(); // you can use rvalues if needed, like
+                               // `auto value = std::move(ret).value();`
+    // Output: mmcjson
+    std::cout << value["repo"].as_string() << std::endl;
+
+    /* Output:
+        MagicstMagoo's homepage: https://github.com/MagicstMagoo
+    */
+    for (auto&& [name, homepage] : value["author"].as_object()) {
+        std::cout << name << "'s homepage: " << homepage.as_string() << std::endl;
+    }
+
+    // Output: abc
+    std::string str = (std::string)value["str"];    // it is equivalent to `value["str"].as_string()`
+    std::cout << str << std::endl;
+
+    // Output: 3.141600
+    double num = value["num"].as_double();          // similarly, you can use `(double)value["num"]`
+    std::cout << num << std::endl;
+
+    // Output: default_value
+    std::string get = value.get("maybe_exists", "default_value");
+    std::cout << get << std::endl;
+
+    // Output: you found me!
+    std::string nested_get = value.get("A_obj", "B_arr", 0, "C_str", "default_value");
+    std::cout << nested_get << std::endl;
+
+    /*  Output:
+        1
+        2
+        3
+    */
+    // It's const!
+    for (const auto& ele : value.at("list").as_array()) {
+        int x = (int)ele;
+        std::cout << x << std::endl;
+    }
+}
+
+void serializing()
+{
+    json::value root;
+    root["hello"] = "mmcjson";
+    root["Pi"] = 3.1416;
+
+    root["arr"] = json::array{
+        "a", "b", "c"
+    };
+    root["obj"] = json::object{
+        {"obj_key1", "aaa"},
+        {"obj_key2", 123},
+        {"obj_key3", true}
+    };
+    root["obj"].object_emplace("key4", json::object{ { "key4 child", "lol" } });
+    root["obj_another"]["child"]["grand"] = "i am grand";
+
+    std::vector<int> vec = { 1, 2, 3, 4, 5 };
+    root["arr from vec"] = json::array(vec);
+    root["arr from vec"].array_emplace(6);
+
+    root["arr from vec"] += json::array{ 7, 8, 9, 10 };
+
+    std::set<std::string> set = { "a", "bbb", "cc" };
+    root["arr from set"] = json::array(set);
+
+    std::map<std::string, int> map;
+    map.emplace("key1", 1);
+    map.emplace("key2", 2);
+    root["obj from map"] = json::object(map);
+
+    auto other = json::object{
+        { "other_key", "lol" },
+        { "obj", "Existing key will not be overwritten"}
+    };
+    // take union
+    root |= other;
+
+    std::cout << root.format() << std::endl;
 }
